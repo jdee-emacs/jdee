672f632a1a87a2b7f36fa4526e5aa8643d91db87
shyamalprasad <shyamalprasad>
2013-04-26 16:19:24 +0000

    Max CEDET version should be 2.0 to support GNU Emacs 24.3.1.
    git-svn-id: https://svn.code.sf.net/p/jdee/code/trunk/jdee@281 381a1f10-3442-0410-997e-ef331cd04102

--- lisp/jde.el
+++ lisp/jde.el
@@ -40,7 +40,7 @@
 
 (defconst jde-cedet-min-version "1.0beta2"
   "Cedet minimum version")
-(defconst jde-cedet-max-version "1.1"
+(defconst jde-cedet-max-version "2.0"
   "Cedet maximum version")
 
 (defconst jde-xemacsp (string-match "XEmacs" (emacs-version))
