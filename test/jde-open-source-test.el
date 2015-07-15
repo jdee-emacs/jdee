8283c9e621fe33872c05d400157a77360890023c
Przemysław Wojnowski <esperanto@cumego.com>
2015-07-15 23:27:37 +0200

    Rename tests to have consistent naming
    Silence flycheck warnings.

--- test/jde-open-source-test.el
+++ test/jde-open-source-test.el
@@ -1,8 +1,15 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
 (require 'ert)
 
 (require 'jde-autoload)
 (require 'beanshell)
 (require 'jde-open-source)
 
-(ert-deftest jde-remove-type-test ()
+(ert-deftest test-jde-remove-type ()
   (should (equal '("List") (jde-remove-type '("List<String>")))))
+
+(provide 'jde-open-source-test)
+;;; jde-open-source-test.el ends here
--- test/jde-util-test.el
+++ test/jde-util-test.el
@@ -1,6 +1,13 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+
 (require 'ert)
 
 (require 'jde-util)
 
-(ert-deftest jde-create-default-prompt-test ()
+(ert-deftest test-jde-create-default-prompt ()
   (should (string= "a (default b): " (jde-create-default-prompt "a" "b"))))
+
+(provide 'jde-util-test)
+;;; jde-util-test.el ends here
