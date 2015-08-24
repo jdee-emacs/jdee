28948382ad1dae378713302094dceb65f9c31898
Przemysław Wojnowski <esperanto@cumego.com>
2015-08-24 11:10:32 +0200

    Correct flycheck errors

--- which.el
+++ which.el
@@ -1,5 +1,4 @@
-;; which.el - UNIX command line `which' like library
-;; $Id$
+;; which.el --- UNIX command line `which' like library
 
 ;; Author: Paul Kinnucan <paulk@mathworks.com>
 ;; Maintainer: Paul Landes <landes <at> mailc dt net>
@@ -23,62 +22,69 @@
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
-(defvar windows-suffixes 
+;;; Commentary:
+
+;;; Code:
+
+(defvar windows-suffixes
   (if (memq system-type (list 'ms-dos 'windows-nt))
       (list ".exe" ".EXE" ".cmd" ".CMD" ".bat" ".BAT" "")
     (list ""))
-  "List of suffixes used by Windows executables")
+  "List of suffixes used by Windows executables.")
 
-(defun which (exe &optional insert &optional silent) 
-  "Show the full path name of an executable.
-With a prefix argument, insert the full-path name at point.
+(defun which (exe &optional insert &optional silent)
+  "Show the full path name of an executable `EXE'.
+With a prefix argument, `INSERT' the full-path name at point.
+Use `SILENT' to suppress messages.
 This command searches the directories in `exec-path'."
   (interactive "sWhich: \nP")
   (let ((executable (which-find-executable exe exec-path)))
     (if (not executable)
-(or silent (message "Can't find %s in search path" exe))
+        (or silent (message "Can't find %s in search path" exe))
       (and insert
-   (insert executable))
+           (insert executable))
       (or silent (message "%s is %s" exe executable))
       executable)))
 
-(defun which-find-executable (exe directory-list) 
-  "Show the full path name of an executable in DIRECTORY-LIST."
+(defun which-find-executable (exe directory-list)
+  "Show the full path name of an executable `EXE' in `DIRECTORY-LIST'."
   (catch 'answer
     (mapc
      #'(lambda (dir)
-(mapcar
-#'(lambda (suf)
-    (let ((try (expand-file-name (concat exe suf) dir)))
-      (and (file-executable-p try)
-   (null (file-directory-p try))
-   (throw 'answer try))))
-windows-suffixes))
+         (mapcar
+          #'(lambda (suf)
+              (let ((try (expand-file-name (concat exe suf) dir)))
+                (and (file-executable-p try)
+                     (null (file-directory-p try))
+                     (throw 'answer try))))
+          windows-suffixes))
      directory-list)
     nil))
 
-(defun which-find-all-executables (exe directory-list) 
-  "Show the full path name of an executable in DIRECTORY-LIST."
+(defun which-find-all-executables (exe directory-list)
+  "Show the full path name of an executable `EXE' in DIRECTORY-LIST."
   (let ((answers))
     (mapc
      #'(lambda (dir)
-(mapc
-#'(lambda (suf)
-    (let ((try (expand-file-name (concat exe suf) dir)))
-      (and (file-executable-p try)
-   (null (file-directory-p try))
-   (setq answers (cons try answers))
-   )))
-windows-suffixes))
+         (mapc
+          #'(lambda (suf)
+              (let ((try (expand-file-name (concat exe suf) dir)))
+                (and (file-executable-p try)
+                     (null (file-directory-p try))
+                     (setq answers (cons try answers))
+                     )))
+          windows-suffixes))
      directory-list)
     answers
     ))
 
 (defun which-find-file (exe)
-  "Find an executable file from `exec-path'."
+  "Find an executable `EXE' file from `exec-path'."
   (interactive "sWhich: ")
   (let ((file (which exe nil t)))
     (and file
 (find-file file))))
 
 (provide 'which)
+
+;;; which.el ends here
