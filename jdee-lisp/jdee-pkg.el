48364123c70d7101268041d9c6b7dd97c5b74fa9
Troy Daniels <udalrich.schermer@gmail.com>
2015-12-30 23:30:07 -0500

    Add more dependencies

--- jdee-lisp/jdee-pkg.el
+++ jdee-lisp/jdee-pkg.el
@@ -4,7 +4,9 @@
   "jdee"
   "2.4"
   "Java Development Environment for Emacs"
-  '((cider "0.11"))
+  '((cider "0.11")
+    (load-relative "1.2")
+    (dash "20151216.1315"))
   :url "http://github.com/jdee-emacs/jdee.git"
   :keywords '("java" "tools"))
 
