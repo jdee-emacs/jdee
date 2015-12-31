ca8c17352c29d1136007113c03ff8444af6b0523
Troy Daniels <udalrich.schermer@gmail.com>
2015-12-30 23:04:47 -0500

    Add package definition for MELPA

---
+++ jdee-lisp/jdee-pkg.el
@@ -0,0 +1,14 @@
+;;; jdee-pkg.el --- Java Development Environment for Emacs
+
+(define-package
+  "jdee"
+  "2.4"
+  "Java Development Environment for Emacs"
+  (cider)
+  :url "http://github.com/jdee-emacs/jdee.git"
+  :keywords '("java" "tools"))
+
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
