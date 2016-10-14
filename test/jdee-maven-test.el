0d862adc34799f1013da4d55848968c6e95470fd
Matthew O. Smith <matthew.smith@imail.org>
2016-10-14 09:05:28 -0600

    Added unit test for jdee-maven-scope-file

--- test/jdee-maven-test.el
+++ test/jdee-maven-test.el
@@ -37,5 +37,24 @@ doesn't try to hit the file system."
           (should (string= (format "%s/" (car (nth 2 dirs)))
                            (jdee-maven-get-default-directory))))))))
 
+
+(ert-deftest test-maven-jdee-scope-file-with-expected-paths ()
+  "Check that `jdee-maven-scope-file' can find the right scope for the path"
+  (let ((source-path "/a/b/c/src/main/java/d/e/f/G.java")
+        (test-path "/a/b/c/src/test/java/d/e/f/G.java"))
+    (should (eq 'compile (car (jdee-maven-scope-file source-path))))
+    (should (eq 'compile (car (let ((default-directory source-path)) (jdee-maven-scope-file)))))
+    (should (eq 'test (car (jdee-maven-scope-file test-path))))
+    (should (eq 'test (car (let ((default-directory test-path)) (jdee-maven-scope-file)))))))
+
+(ert-deftest test-maven-jdee-scope-file-with-non-maven-paths ()
+  "Check that `jdee-maven-scope-file' returns nil for non-maven paths"
+  (let ((source-path "/a/b/c/d/e/f/G.java")
+        (test-path "/a/b/c/d/e/f/G.java"))
+    (should (null (jdee-maven-scope-file source-path)))
+    (should (null (let ((default-directory source-path)) (jdee-maven-scope-file))))
+    (should (null (jdee-maven-scope-file test-path)))
+    (should (null (let ((default-directory test-path)) (jdee-maven-scope-file))))))
+
 (provide 'jdee-maven-test)
 ;;; jdee-maven-test.el ends here
