75c5fd7abb53b6b835dd4227004ceceddc2f6b34
Przemysław Wojnowski <esperanto@cumego.com>
2017-01-08 17:11:34 +0100

    Move JDEE activation/deactivation to separate file.

--- jdee.el
+++ jdee.el
@@ -42,6 +42,7 @@
 (require 'easymenu)
 (require 'executable)
 (require 'jdee-log)
+(require 'jdee-activator)
 (require 'jdee-abbrev)
 (require 'jdee-annotations)
 (require 'jdee-bug)
@@ -274,9 +275,6 @@ This command invokes the function defined by `jdee-build-function'."
   (interactive)
   (call-interactively jdee-build-function))
 
-(defvar jdee-monitor-post-command-hook-timer nil
-  "Timer that runs `jdee-monitor-post-command-hook' during idle moments.")
-
 ;; The following is the expansion of the above macro.
 ;; We include the expansion to permit autoload on jdee-mode.
 (derived-mode-init-mode-variables 'jdee-mode)
@@ -319,23 +317,7 @@ This command invokes the function defined by `jdee-build-function'."
 	(if (and (not (jdee-debugger-running-p)) jdee-project-context-switching-enabled-p)
 	    (jdee-load-project-file))
 
-	;; Enable support for automatic project switching.
-	;; This feature loads the appropriate project settings whenever
-	;; a user switches from a Java buffer belonging to one project
-	;; to a buffer belonging to another.
-	(add-hook 'post-command-hook
-		  'jdee-detect-java-buffer-activation
-		  nil
-		  t ;; XEmacs ignores this argument if symbol is not local.
-		  )
-
-	(unless jdee-monitor-post-command-hook-timer
-	  (setq
-	   jdee-monitor-post-command-hook-timer
-	   (run-with-idle-timer 1 t 'jdee-monitor-post-command-hook)))
-
-	(unless (member 'jdee-clean-up-after-jde kill-buffer-hook)
-	  (add-hook 'kill-buffer-hook 'jdee-clean-up-after-jde))
+        (jdee-activator-init)
 
         ;; Define underscore as a word constituent. This is needed
 	;; to support coding styles the begin fields with an underscore.
@@ -846,65 +828,6 @@ replaces with slashes."
 	     (let ((directory-sep-char ?/))
                (expand-file-name path)))
 	   (split-string cp jdee-classpath-separator))))))
-
-(defvar jdee-entering-java-buffer-hook
-  '(jdee-reload-project-file
-    jdee-which-method-update-on-entering-buffer)
-   "*Lists functions to run when entering a jdee-mode source buffer from another
-jdee-mode buffer. Note that these functions do not run when reentering the same
-jdee-mode buffer from a non-jdee-mode buffer. You should use this hook only for
-functions that need to be run when you switch from one jdee-mode buffer to
-a different jdee-mode buffer. Use `jdee-mode-hook' if the function needs to run
-only once, when the buffer is created.")
-
-(defvar jdee-current-buffer (current-buffer)
-  "*Internal JDEE variable that holds the current active buffer.")
-
-(defun jdee-detect-java-buffer-activation ()
-  "Detects when a user activates a buffer.
-If the activated buffer is a Java buffer, runs the
-`jdee-entering-java-buffer-hook' hooks."
-  (let ((curr-buff (current-buffer)))
-    (unless (equal curr-buff jdee-current-buffer)
-      (setq jdee-current-buffer curr-buff)
-      (if (eq major-mode 'jdee-mode)
-	  (condition-case err
-	      (run-hooks 'jdee-entering-java-buffer-hook)
-	    (error
-	     (message "jdee-entering-java-buffer-hook error: %s"
-		      (error-message-string err))))))))
-
-(defun jdee-monitor-post-command-hook ()
-  "Checks whether `post-command-hook' includes all hooks required
-by JDEE. If not, it adds the required hooks."
-  (if (eq major-mode 'jdee-mode)
-      (dolist (hook (list 'jdee-detect-java-buffer-activation))
-	(when (not (member hook post-command-hook))
-	  (add-hook 'post-command-hook hook)))))
-
-(defun jdee-count-open-java-buffers ()
-  "Returns non-nil if any java buffers are open."
-  (cl-count
-   ".java"
-   (buffer-list)
-   :test
-   (lambda (file-type buffer)
-     (let ((file-name (buffer-file-name buffer)))
-       (if file-name
-	   (save-match-data
-	    (string-match file-type file-name)))))))
-
-
-(defun jdee-clean-up-after-jde ()
-  "Removes `jdee-detect-java-buffer-activation-hook' when
-all Java source buffers have been closed."
-  (if  (eq major-mode 'jdee-mode)
-      (unless  (> (jdee-count-open-java-buffers) 1)
-	(remove-hook 'post-command-hook 'jdee-detect-java-buffer-activation)
-	(when jdee-monitor-post-command-hook-timer
-	  (cancel-timer jdee-monitor-post-command-hook-timer)
-	  (setq jdee-monitor-post-command-hook-timer nil))
-	(remove-hook 'kill-buffer-hook 'jdee-clean-up-after-jde))))
 
 ;; Contributed by John Ciolfi, jciolfi@mathworks.com.
 (defun jdee-compile-file-if-necessary (file)
