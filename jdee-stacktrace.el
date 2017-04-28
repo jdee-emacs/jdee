6bba0d01caed4ddaab0f38673418a8a4967c9dbe
Przemysław Wojnowski <esperanto@cumego.com>
2017-04-28 22:41:21 +0200

    Remove package headers that prevent correct installation from buffer
    package-install-from-buffer was using the last found package headers, which were
    from jdee-stacktrace.
    Reformat docs.

--- jdee-stacktrace.el
+++ jdee-stacktrace.el
@@ -2,10 +2,6 @@
 
 ;; Author: Matthew O. Smith <matt@m0smith.com>
 ;; Maintainer: Paul Landes
-;; Keywords: java, tools
-;; URL: http://github.com/jdee-emacs/jdee
-;; Version: 2.4.2
-;; Package-Requires: ((emacs "24.3"))
 
 ;; Copyright (C) 1997-2008 Paul Kinnucan.
 ;; Copyright (C) 2009 by Paul Landes
@@ -26,27 +22,22 @@
 ;; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
-;;
-;; minimum Emacs version supported is 24.3
 
 ;;; Code:
 
-
 (defun jdee-stacktrace-file ()
-  "A function for use in `compilation-error-regexp-alist' as the
-file name.  Expects (match-string 1) to return the fully
-qualified name of the class."
+  "A function for use in `compilation-error-regexp-alist' as the file name.
+Expects (match-string 1) to return the fully qualified name of the class."
   (jdee-stacktrace-file* (match-string 1)))
 
 (defun jdee-stacktrace-file* (fqn)
-  "Return the full path to the source file for FQN (fully
-qualified name).  If not found, return FQN."
+  "Return the full path to the source file for FQN (fully qualified name).
+If not found, return FQN."
   (let* ((path (save-match-data (jdee-find-class-source-file fqn))))
     (cond
      ((bufferp path) (with-current-buffer path buffer-file-name))
      ((stringp path) path)
      (t fqn))))
-
 
 (defun jdee-stacktrace-item-re ()
   "Match regions set:
@@ -55,33 +46,30 @@ qualified name).  If not found, return FQN."
    3 - class name
    4 - method name
    5 - file name
-   6 - line number
-    "
+   6 - line number"
   (format "at %s[.]%s(%s:\\([[:digit:]]+\\))"
           (jdee-parse-java-fqn-re)   ; FQN
           (jdee-parse-java-name-part-re)    ; Method
           (jdee-parse-java-name-parts-re)))  ; file name
 
-
 (define-derived-mode jdee-stacktrace-mode compilation-mode "JVM Stack Trace"
   "Major mode for inspecting stack traces.  Expects
 `jdee-sourcepath' to be set appropriately"
-  ;; FIXME: Should be a minor mode 
+  ;; FIXME: Should be a minor mode
   (make-local-variable 'compilation-error-regexp-alist)
   (setq compilation-error-regexp-alist
         (list
          (list
-         (jdee-stacktrace-item-re)
-         1 ;'jdee-stacktrace-file
-         6 nil nil
-         1
-         '(4 compilation-info-face)
-         '(2 compilation-error-face)))))
+          (jdee-stacktrace-item-re)
+          1 ;'jdee-stacktrace-file
+          6 nil nil
+          1
+          '(4 compilation-info-face)
+          '(2 compilation-error-face)))))
 
-
-(defun jdee-stacktrace-buffer ( clear &optional buffer)
-  "Open a buffer to paste a stack trace.  Parses the stack trace
-to allow editting of the source.
+(defun jdee-stacktrace-buffer (clear &optional buffer)
+  "Open a buffer to paste a stack trace.
+Parses the stack trace to allow editting of the source.
 
 If called with a prefix, will clear the stake trace buffer.
 
