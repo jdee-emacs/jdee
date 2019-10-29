a1741931e15fbb8b1403309ddaeda41d728f6d79
Lei Zhao <LeiZh26@gmail.com>
2019-10-29 16:36:24 +0800

    Update .dir-locals.el
    Simplify `.dir-locals.el` to make it more human readable.  And add
    `(lisp-indent-function . lisp-indent-function)` to it.  However, you
    may also need to add this to your `safe-local-variable-values`.  This
    is definitely safe, since this is actually the default value for
    Emacs.  Many Lisp programmers like to set the indent function to
    something else like `common-lisp-indent-function`.  This way, they
    don't have to change this variable back and forth when switching
    projects and it's likely for this project to get a consistent
    indentation.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,6 @@
-((emacs-lisp-mode . ((indent-tabs-mode . nil))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode
+  (indent-tabs-mode . nil)
+  (lisp-indent-function . lisp-indent-function)))
